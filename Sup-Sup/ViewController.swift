c3894e000a12c4533b74f384f6abba9b9058bced
Dima Miro <d.miro@macbook-pro-dima.local>
2018-10-15 23:08:02 +0200

    Initial Commit

---
+++ Sup-Sup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sup-Sup
+//
+//  Created by Dima Miro on 15/10/2018.
+//  Copyright © 2018 Dima Miro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
